cb120e52db623add5aa7f3fdd034250c761adf30
Szymon Kruszynski <kruszon4@gmail.com>
2019-02-13 14:53:31 +0100

    First Commit

---
+++ src/main/java/com/english/WordApp/WordAppApplication.java
@@ -0,0 +1,14 @@
+package com.english.WordApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordAppApplication.class, args);
+	}
+
+}
+
